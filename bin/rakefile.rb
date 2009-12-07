95f24043a1786175f1487110cf77cf6b342b68d6
wtnabe <wtnabe@gmail.com>
2009-12-07 19:15:21 +0900

    copied `untracked' task from http://gist.github.com/213791

--- bin/rakefile.rb
+++ bin/rakefile.rb
@@ -1,5 +1,38 @@
 # -*- mode: ruby; coding: utf-8 -*-
 
+module SCM
+  %w( svn hg git ).each { |scm|
+    define_method( "scm_#{scm}?".to_sym ) {
+      File.exist?( ".#{scm}" ) and which( scm )
+    }
+  }
+end
+include SCM
+
+def which( cmd )
+  path = ENV['PATH']
+
+  paths = path.split( File::PATH_SEPARATOR ).select { |path|
+    File.exist?( path ) and File.exist?( File.join( path, cmd ) )
+  }
+  if ( paths.size > 0 )
+    paths.first + File::SEPARATOR + cmd
+  else
+    nil
+  end
+end
+
+def cut( cmd, chr = '?' )
+  `#{cmd}`.map { |line|
+    c = line.split( /\s+/ )
+    if ( c[0] =~ Regexp.new( "[#{chr}]" ) )
+      c[c.size-1]
+    else
+      nil
+    end
+  }.compact
+end
+
 def rake_dir
   File.dirname( __FILE__ )
 end
@@ -33,6 +66,19 @@ desc "test all test cases"
 task :testall do
   devel = SimplePhpDevel.new
   devel.exec_test( devel.test_dir )
+end
+
+if ( methods.grep( /\Ascm_/ ).size > 0 )
+  desc "list up untracked files"
+  task :untracked do
+    if ( scm_hg? )
+      puts cut( 'hg stat -A', 'I?' )
+    elsif ( scm_git? )
+      puts `git ls-files -o`
+    elsif ( scm_svn? )
+      puts cut( 'svn stat -v', '?' )
+    end
+  end
 end
 
 task :default => :test do
